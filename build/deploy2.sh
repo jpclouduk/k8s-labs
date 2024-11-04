da36627176d271c1c26ca2385e178bc2e0b5f6ff
jpclouduk <poulsenjx@gmail.com>
2024-11-04 15:16:41 +0000

    update scripts

---
+++ build/deploy2.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+##### ENABLE IP FORWARDING
+# sysctl params required by setup, params persist across reboots
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+# apply sysctl params without reboot
+sudo sysctl --system
+
+# check IP forwarding status
+sysctl net.ipv4.ip_forward
+
+
+##### DEPLOY CONTAINERD
+# remove all docker packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install Containerd
+sudo apt-get install containerd.io
+
+
+##### UPDATE CGROUPS
+# update containerd config
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sed -i 's/systemd_cgroup = false/systemd_cgroup = true/g' /etc/containerd/config.toml
+sed -i 's/pause:3.8/pause:3.10/g' /etc/containerd/config.toml
+
+# restart containerd
+sudo systemctl restart containerd
+
+
+##### Deploy kubeadm, kubelet and kubectl
+# update all repos
+sudo apt-get update
+# update certs and tools. apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# deploy or update the keyring. If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# update or overwrite existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# install all kube components
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# start kubelet
+sudo systemctl enable --now kubelet
